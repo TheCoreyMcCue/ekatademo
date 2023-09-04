bf54e516fa5579a5a95b7f5551e647065c27ddc9
Corey McCue <corey.mccue.com>
2023-09-04 13:28:14 +0200

    initialize app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import RegForm from "./components/Form";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        background: "black",
+        height: "110vh",
+        width: "100vw",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+      className="App"
+    >
+      <RegForm />
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RegForm = () => {
+  return <div style={{ color: "white" }}>Under Construction</div>;
+};
+
+export default RegForm;
