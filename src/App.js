e7d6f52e4dc5d84a0a3bc1e12b5dce459e485f57
Corey McCue <corey.mccue.com>
2023-09-05 11:04:43 +0200

    begin form and finish setup

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div
       style={{
-        background: "black",
+        // background: "black",
         height: "110vh",
         width: "100vw",
         display: "flex",
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,41 @@
 import React from "react";
+import {
+  FormControl,
+  // InputLabel,
+  // Input,
+  TextField,
+  // FormHelperText,
+} from "@mui/material";
 
 const RegForm = () => {
-  return <div style={{ color: "white" }}>Under Construction</div>;
+  return (
+    <div
+      style={{
+        color: "white",
+        display: "flex",
+        flexDirection: "column",
+        width: "20vw",
+      }}
+    >
+      <FormControl>
+        <TextField
+          required
+          id="outlined-required"
+          label="Username"
+          // defaultValue="Hello World"
+        />
+      </FormControl>
+      <FormControl>
+        <TextField
+          id="outlined-password-input"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          style={{ marginTop: "0.5rem" }}
+        />
+      </FormControl>
+    </div>
+  );
 };
 
 export default RegForm;
