219948305fd6e56e672a7b2e10a7836fa38e5ffe
Corey McCue <corey.mccue.com>
2023-09-06 13:39:32 +0200

    ad request page and begin layout for signup page

--- src/pages/ApiRequestPage.jsx
+++ src/pages/ApiRequestPage.jsx
@@ -4,42 +4,6 @@ import { Link } from "react-router-dom";
 import mainLogo from "../assets/Mastercard_2019_logo.png";
 import shareVideo from "../assets/req.mp4";
 
-const delayedButton = () => {
-  setTimeout(
-    <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0    bg-blackOverlay">
-      <div className="p-1">
-        <img src={mainLogo} alt="" width="130px" />
-      </div>
-      <h2 className="pt-5 text-l" style={{ color: "#F89F15" }}>
-        Select a Product
-      </h2>
-      <div className="p-5 text-xl">
-        <Link to="/login">
-          <Button
-            style={{ marginRight: "0.5rem" }}
-            variant="outlined"
-            color="warning"
-          >
-            Account Opening
-          </Button>
-        </Link>
-        <Link to="/cart">
-          <Button
-            style={{ marginLeft: "0.5rem" }}
-            variant="outlined"
-            color="warning"
-          >
-            Transaction Risk
-          </Button>
-        </Link>
-      </div>
-
-      <div className="shadow-2xl"></div>
-    </div>,
-    5000
-  );
-};
-
 const ChooseProduct = () => (
   <div className="flex justify-start items-center flex-col h-screen">
     <div className=" relative w-full h-full">
