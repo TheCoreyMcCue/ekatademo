16fa40ace623e81b0d8e39ac570c7685ef38af9a
Corey McCue <corey.mccue.com>
2023-09-06 10:52:03 +0200

    begin work on selection page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./App.css";
 // import RegForm from "./components/regForm";
-import Signup from "./components/Signup";
+// import Signup from "./components/Signup";
 // import { styled } from "@mui/material/styles";
 // import Box from "@mui/material/Box";
 import { Route, Routes } from "react-router-dom";
-import useMediaQuery from "@mui/material/useMediaQuery";
-import mainLogo from "./assets/Mastercard_2019_logo.png";
-import Landing from "./components/Landing";
+// import useMediaQuery from "@mui/material/useMediaQuery";
+// import mainLogo from "./assets/Mastercard_2019_logo.png";
+// import Landing from "./components/Landing";
 
 import {
   ChooseProduct,
@@ -21,9 +21,9 @@ import {
 } from "./pages";
 
 const App = () => {
-  const Desktop = useMediaQuery("(min-width:1100px)");
-  const Ipad = useMediaQuery("(min-width:760px)");
-  const Mobile = useMediaQuery("(min-width:500px)");
+  // const Desktop = useMediaQuery("(min-width:1100px)");
+  // const Ipad = useMediaQuery("(min-width:760px)");
+  // const Mobile = useMediaQuery("(min-width:500px)");
 
   return (
     // <div style={{ height: "100vh", background: "#dcdfe3" }}>
--- src/pages/ChooseProduct.jsx
+++ src/pages/ChooseProduct.jsx
@@ -1,7 +1,49 @@
 import React from "react";
+import { Button } from "@mui/material";
+import mainLogo from "../assets/Mastercard_2019_logo.png";
+import shareVideo from "../assets/choose.mp4";
 
-const ChooseProduct = () => {
-  return <div>ChooseProduct</div>;
-};
+const ChooseProduct = () => (
+  <div className="flex justify-start items-center flex-col h-screen">
+    <div className=" relative w-full h-full">
+      <video
+        src={shareVideo}
+        type="video/mp4"
+        loop
+        controls={false}
+        muted
+        autoPlay
+        className="w-full h-full object-cover"
+      />
+
+      <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0    bg-blackOverlay">
+        <h2 className="p-5 text-xl" style={{ color: "#F89F15" }}>
+          Select a Product
+        </h2>
+        <div className="p-1">
+          <img src={mainLogo} alt="" width="130px" />
+        </div>
+        <div className="p-5 text-xl">
+          <Button
+            style={{ marginRight: "0.5rem" }}
+            variant="outlined"
+            color="warning"
+          >
+            Account Opening
+          </Button>
+          <Button
+            style={{ marginLeft: "0.5rem" }}
+            variant="outlined"
+            color="warning"
+          >
+            Transaction Risk
+          </Button>
+        </div>
+
+        <div className="shadow-2xl"></div>
+      </div>
+    </div>
+  </div>
+);
 
 export default ChooseProduct;
