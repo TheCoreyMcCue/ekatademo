e0990cbadc91b1358a59111ab68448f757881673
Corey McCue <corey.mccue.com>
2023-09-07 15:44:40 +0200

    last touches to login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -10,11 +10,13 @@ import {
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { FcGoogle } from "react-icons/fc";
+
 const LoginPage = () => {
   const theme = createTheme({
     palette: {
       primary: { main: "#060606" },
-      // secondary: purple,
+      secondary: { main: "#f5f5f5" },
     },
   });
 
@@ -22,7 +24,7 @@ const LoginPage = () => {
     <ThemeProvider theme={theme}>
       <section className="w-full h-screen flex items-start">
         <div className="relative w-1/2 h-full flex flex-col">
-          <div className="absolute top-[12%] left-[12%] flex flex-col">
+          <div className="absolute top-[32%] left-[2%] flex flex-col">
             <h1 className="text-4xl text-white font-bold mt-4">
               Realize Tomorrow,
             </h1>
@@ -30,22 +32,19 @@ const LoginPage = () => {
             <p className="text-base text-white font-normal">
               Become a member today for loads of community benefits
             </p>
-            {/* <Button variant="outlined">
-            <Link to="/">Back</Link>
-          </Button> */}
           </div>
           <img
             className="w-full h-full object-cover"
             alt="Highrise building"
-            src="https://images.unsplash.com/photo-1693924614710-fb0a4865cffb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2787&q=80"
+            src="https://images.unsplash.com/photo-1682686578456-69ae00b0ecbd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2787&q=80"
           />
         </div>
-        <div className="w-1/2 h-full bg-[#F5F5F5] flex flex-col p-20 justify-between">
+        <div className="w-1/2 h-full bg-[#F5F5F5] flex flex-col p-20 justify-between items-center">
           <h1 className="text-xl font-semibold text-[#060606]">
             Interactive Brand
           </h1>
 
-          <div className="w-full flex flex-col max-w-[550px]">
+          <div className="w-full flex flex-col max-w-[400px]">
             <div className="w-full flex flex-col mb-7">
               <h3 className="text-2xl font-semibold mb-2">Login</h3>
               <p className="text-base mb-2">
@@ -94,11 +93,25 @@ const LoginPage = () => {
                 </div>
               </div>
               <div className="w-full flex flex-col my-4">
-                {/* <div> */}
-                <Button className="w-full" variant="contained">
-                  Success
+                <Button className="w-full my-2" variant="contained">
+                  Login
                 </Button>
-                {/* </div> */}
+              </div>
+              <div className="w-full flex items-center justify-center relative py-2">
+                <div className="w-full h-[1px] bg-black"></div>
+                <p className="text-lg  absolute text-black/80 bg-[#f5f5f5]">
+                  or
+                </p>
+              </div>
+              <div className="w-full flex flex-col my-4">
+                <Button
+                  className="w-full my-2"
+                  color="secondary"
+                  variant="contained"
+                >
+                  Login with{" "}
+                  <FcGoogle style={{ marginLeft: "5px", fontSize: "18px" }} />
+                </Button>
               </div>
             </div>
           </div>
@@ -106,12 +119,10 @@ const LoginPage = () => {
           <div className="w-full flex items-center justify-center">
             <p className="text-small font-normal text-[#060606]">
               Don't have an account?{" "}
-              {/* <span className="border-[10px] border-[#F89F15]"> */}
               <Link className="underline" to="/signup">
                 Signup for free
               </Link>
-              {/* <br /> <Link to="/">⬅️ navigate to home page</Link> */}
-              {/* </span> */}
+              <br /> <Link to="/">⬅️ navigate to home page</Link>
             </p>
           </div>
         </div>
