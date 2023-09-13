2e84334157ce937e2f4262b9fdd78136b6cf0dc6
Corey McCue <corey.mccue.com>
2023-09-13 11:42:15 +0200

    navbar update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ import MenuItem from "@mui/material/MenuItem";
 // import AdbIcon from "../assets/Mastercard_2019_logo.png";
 
 const pages = ["Products", "Pricing", "Blog"];
-const settings = ["Profile", "Account", "Dashboard", "Logout"];
+const settings = ["Login"];
 
 function ResponsiveAppBar() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
